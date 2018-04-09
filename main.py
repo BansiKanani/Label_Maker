b0234cffe2bc7ee32a300661643b8325c1bfafea
Bansikumar Kanani <33679038+BansiKumarKanani@users.noreply.github.com>
2018-04-09 23:32:44 +0530

    I've put font files to source directory.

--- main.py
+++ main.py
@@ -3,79 +3,111 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import xlrd
 import os
-import random
+import time
 
 def xls_to_list(xlsfile):
 
-    dataList = [] #list = [lbl_NO, Type, Length, Width]
+    #list = [Label_NO, Type, Length, Width, top, left, bottom, right]
+    dataList = []
+
+    #Open workbook and select sheet
     workBook = xlrd.open_workbook(xlsfile)
     sheet = workBook.sheet_by_index(0)
-    for row in range(2, sheet.nrows):
-        dataList.append([sheet.cell_value(row, 9), (sheet.cell_value(row, 8)).upper(), sheet.cell_value(row, 10), sheet.cell_value(row, 11)])
-        dataList.append([sheet.cell_value(row, 12), (sheet.cell_value(row, 8)).upper(), sheet.cell_value(row, 13), sheet.cell_value(row, 14)])
 
+    #read all the available data
+    for row in range(2, sheet.nrows):
+
+        #Remove .0 from the Excel data
+        tempStr = str(int(sheet.cell_value(row, 9)))
+        for i in range(2):
+            dataList.append([tempStr, (sheet.cell_value(row, 8)).upper(),
+                             str(int(sheet.cell_value(row, 10))), str(int(sheet.cell_value(row, 11))),
+                             str(sheet.cell_value(row, 15)), str(sheet.cell_value(row, 16)),
+                             str(sheet.cell_value(row, 17)), str(sheet.cell_value(row, 18))])
+        for i in range(2):
+            dataList.append([sheet.cell_value(row, 12), (sheet.cell_value(row, 8)).upper(),
+                             str(int(sheet.cell_value(row, 13))), str(int(sheet.cell_value(row, 14))),
+                             str(sheet.cell_value(row, 15)), str(sheet.cell_value(row, 16)),
+                             str(sheet.cell_value(row, 17)), str(sheet.cell_value(row, 18))])
+    print(' XLSX data fetched!')
     return dataList
 
-def make_label(lblDataList):
 
+def make_label(lblDataList, font_file, tf, ff):
+
+    print (' Making Labels from data...')
     allImages = []
-    fontNumber = ImageFont.truetype("font/Consolas.ttf", 80)
-    fontText = ImageFont.truetype("font/Consolas.ttf", 50)
+
+    #select fonts
+    fontNumber = ImageFont.truetype(font_file, 80)
+    fontText = ImageFont.truetype(font_file, 50)
+    fontSize = ImageFont.truetype(font_file, 40)
 
     for lbl in lblDataList:
 
-        if type(lbl[0]) == float: lblTemp = str(int(lbl[0]))
-        else: lblTemp = str(lbl[0])
+        tff = ''
+        if lbl[1] == 'T':
+            if 'A' in lbl[0]:
+                tff=ff
+            else:
+                tff=tf
+        elif lbl[1] =='F':
+            if 'A' in lbl[0]:
+                tff=tf
+            else:
+                tff=ff
 
-        if lbl[1] == 'T': img = Image.open("source/t.png")
-        elif lbl[1] =='F': img = Image.open("source/f.png")
+        img = Image.open(tff)
         draw = ImageDraw.Draw(img)
 
-        draw.text((260, 140), lblTemp+'-('+lbl[1].upper()+')', (0,0,0), font=fontNumber)
-        draw.text((260, 260), str(int(lbl[2]))+' X '+str(int(lbl[3])), (0,0,0), font=fontText)
+        #Print label data on images
+        draw.text((260, 140), lbl[0]+'-('+lbl[1]+')', (0,0,0), font=fontNumber) #Label number, Type
+        draw.text((260, 260), lbl[2]+' X '+lbl[3], (0,0,0), font=fontText) #size
 
-        #print(lblTemp+'-('+lbl[1].upper()+')')
-        #print(str(lbl[2])+'x'+str(lbl[3]))
-
-
-
-        #draw.text((400, 150), '-'+lbl[1].upper(), (0,0,0), font=fontText)
-        #img.save('temp/{0}-{1}.jpg'.format(i[0], i[1]))
+        draw.text((380, 15), lbl[4], (0,0,0), font=fontSize) #top
+        draw.text((30, 200), lbl[5], (0,0,0), font=fontSize) #left
+        draw.text((380, 390), lbl[6], (0,0,0), font=fontSize) #bottom
+        draw.text((740, 200), lbl[7], (0,0,0), font=fontSize) #right
         allImages.append(img)
 
+    print(' Labels are made!')
     return allImages
+
 
 def merge_images(allImages):
 
+    print(' Making PDF...')
     (width, height) = allImages[0].size
-    mixedList = []
+    #total counter counts all iteration / inPageCounter count images in each page / pages stores number
     totalCounter, inPageCounter, pages = 0, 0, 1
 
-    while totalCounter < len(allImages): #do for all images
+    #Do for all images
+    while totalCounter < len(allImages):
+        #Create while image to print labels in it.
         mixedImg = Image.new('RGB', (3 * width, 8 * height), (255,255,255))
-        while inPageCounter < 24:    #check if page is full, if so then add to list.
+        #Check if page is full then reset inImgCounter.
+        while inPageCounter < 24:
             for row in range(8):
                 for col in range(3):
                     if totalCounter < len(allImages):
                         mixedImg.paste(im=allImages[totalCounter], box=(col*width, row*height))
                     totalCounter += 1
                     inPageCounter += 1
-        #mixedImg.save('temp/{}.png'.format(pages))
-        mixedImg.save('temp/{}.pdf'.format(pages), 'PDF', resolution=100.0)
+
+        mixedImg.save('Label-{}.pdf'.format(pages), 'PDF', resolution=100.0)
         pages += 1
         inPageCounter = 0
+        #Delete old image from memory.
         mixedImg.close()
 
-    return mixedList
+    print('-------DONE--------\n You may now close this window!')
+    time.sleep(5)
 
+excel_file = r'LabelData.xlsx'
+font_file = r'source/Consolas.ttf'
+t_image = r'source/t.png'
+f_image = r'source/f.png'
 
-
-#lblType = ['t', 'f']
-#lblRange = range(1, random.randint(1, 500))
-#rand_items = []
-#for i in lblRange: rand_items.append([i, lblType[random.randint(0,1)]])
-
-file_location = 'TestData.xlsx'
-xlsList = xls_to_list(file_location) #return LIST containing xls Data
-allImages = make_label(xlsList) #return LIST containing Image Objects
+dataList = xls_to_list(excel_file) #return LIST containing xls Data
+allImages = make_label(dataList, font_file, t_image, f_image) #return LIST containing Image Objects
 merge_images(allImages)
